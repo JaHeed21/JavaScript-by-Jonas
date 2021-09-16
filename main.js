43e29057c7bcc6d13b2efa0bd2f8d5e62b766714
jaheed21 <jahidulrayhan2002@gmail.com>
2021-09-16 21:29:36 +0600

    Match result

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ while (i >= -50 && i <= 50) {
 
 let dice = Math.trunc(Math.random() * 6) + 1; */
 
-
+/* 
 const calTip = function (bill) {
     return bill >= 50 && bill <= 300 ? bill * .15 : bill * .20;
 };
@@ -50,11 +50,35 @@ for (i = 0; i < ammount.length; i++) {
 }
 console.log(ammount, profit, totals);
 
+ */
+/* 
+let i = Math.trunc(Math.random() * 50) + 1;
+while (i !== 50) {
+    document.write(`Random Number between 1-50 = ${i}`);
+    console.log(`Random Number between 1-50 = ${i}`);
+    i = Math.trunc(Math.random() * 50) + 1;
+    if (i === 50) {
+        console.log(`Congrates! You got ${i} as random number.`)
+    }
+};
+ */
+const teamDolphin = [99, 102, 87, 128];
+const teamKolas = [101, 98, 89, 128];
 
-let sum = Math.trunc(Math.random() * 6) + 1;
-i = 0;
-while (i <= 19) {
-    console.log(`repliacation ${i}`);
+
+const DAv = function (a, b, c, d) {
+    return (a + b + c + d) / 4;
+};
+const ScoreDolphin = DAv(teamDolphin[0], teamDolphin[1], teamDolphin[2], teamDolphin[3]);
+const ScoreKolas = DAv(teamKolas[0], teamKolas[1], teamKolas[2], teamKolas[3], );
+
+console.log(ScoreDolphin, ScoreKolas);
+
+if (ScoreKolas > ScoreDolphin) {
+    console.log(`Team kolas wins with avarage score ${ScoreKolas}`)
+} else if (ScoreDolphin === ScoreKolas) {
+    console.log(`match draw with avarage ${ScoreKolas} = ${ScoreDolphin}.`)
+} else {
+    console.log(`Team Dolphin wins with avarage score${ScoreDolphin}`)
 
 }
-sum = Math.trunc(Math.random() * 6) + 1;
