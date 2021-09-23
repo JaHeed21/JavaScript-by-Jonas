818d23d695f7effd90a0abac625dd7ea390b4db1
JaHeed21 <jahidulrayhan2002@gmail.com>
2021-09-23 09:24:10 +0600

    Html Documenet

--- main.js
+++ main.js
@@ -159,25 +159,6 @@ const kalvinMeasure = function () {
 console.log(kalvinMeasure()); */
 
 const day = prompt('Enter a week day to know what i do that day.');
-switch (day) {
-  case 'monday':
-    console.log('plan course structure');
-    console.log('Go to Coading meetup');
-    break;
-  case 'tuesday':
-    console.log('Write script for videos.');
-    break;
-  case 'wednesday':
-  case 'thursaday':
-    console.log('Make videos on script and upload on my channel');
-    break;
-  case 'friday':
-  case 'saturday':
-    console.log('enjoy my weekend');
-    break;
-  default:
-    console.log('what you enter its not a valid day.');
-}
 
 if (day == 'monday') {
   console.log('plan course structure');
